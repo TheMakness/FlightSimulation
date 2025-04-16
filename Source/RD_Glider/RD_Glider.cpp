ac17f82bb7bfdccebd7f173dd87b23efa0fbb345
TheMakness <44522330+TheMakness@users.noreply.github.com>
2025-04-17 00:43:08 +0200

    Update flight parameters

---
+++ Source/RD_Glider/RD_Glider.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RD_Glider.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RD_Glider, "RD_Glider" );
